90e9dfa27b31d77baeb937195d25dfe537f1953f
Ankit Gupta <51711028+ankitjss503251@users.noreply.github.com>
2022-11-23 18:50:56 +0530

    Update installation_tools.sh

--- installation_tools.sh
+++ installation_tools.sh
@@ -40,6 +40,7 @@ sudo java -version
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
+sudo apt-get install jenkins
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins 
